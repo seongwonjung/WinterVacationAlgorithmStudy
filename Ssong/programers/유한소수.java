6a78341e21b42c264d317b6e9ef71cf93ef1653f
yeonkyusong <songsj2040@Naver.com>
2025-01-19 17:18:53 +0900

    Add level0

---
+++ Ssong/programers/유한소수.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int solution(int a, int b) {
+        int answer = 1;
+        int num = 0;
+        for(int i = 1; i <= a; i++){
+            if(a % i == 0 && b % i ==0) 
+            num = i;
+        }
+
+        if(num != 0){
+            b /= num;
+        }
+        while(b > 1){
+            if(b % 2 == 0){
+                b /= 2;
+            }else if(b % 5 == 0){
+                b /= 5;
+            }else {
+                return 2;
+            }
+        }
+
+        return answer;
+    }
+}
