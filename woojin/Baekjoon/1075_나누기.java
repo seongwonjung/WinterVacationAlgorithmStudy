ffc486049172aaf93dcf8fef86f4875ce6cbb8d3
w00jinLee <leewoojin001121@gmail.com>
2025-01-28 18:10:35 +0900

    Bronze2-1075(나누기)

---
+++ woojin/Baekjoon/1075_나누기.java
@@ -0,0 +1,33 @@
+package woojin.Baekjoon;
+
+import java.util.Scanner;
+
+class cal1075 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int F = sc.nextInt();
+        int x = N%100;
+        int result = N-x;
+
+        sc.close();
+
+        while(result%F!=0){
+            result++;
+        }
+        
+        result=result%100;
+    
+        String resulString = Integer.toString(result);
+
+
+        if(result<10){
+            System.out.println("0"+(resulString));
+        }
+        else{
+            System.out.println(resulString);
+        }
+
+    }
+}
