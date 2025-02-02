d32fe0fc8fd95d13af51b0c51784c79bf4a2f0d6
w00jinLee <leewoojin001121@gmail.com>
2025-02-02 16:08:06 +0900

    level1-같은 숫자는 싫어

---
+++ woojin/Programmers/level1-같은 숫자는 싫어.java
@@ -0,0 +1,25 @@
+package woojin.Programmers;
+
+import java.util.*;
+
+class Solution {
+    public int[] solution(int []arr) {
+        
+        Stack<Integer> stack = new Stack<Integer>();
+        stack.add(arr[0]);
+        
+        for(int i=1; i<arr.length; i++){
+            if(arr[i]!=stack.peek()){
+                stack.add(arr[i]);
+            }
+        }
+        
+        int answer[] = new int[stack.size()];
+        
+        for(int i=0; i<stack.size(); i++){
+            answer[i]=stack.get(i);   
+        }
+
+        return answer;
+    }
+}
