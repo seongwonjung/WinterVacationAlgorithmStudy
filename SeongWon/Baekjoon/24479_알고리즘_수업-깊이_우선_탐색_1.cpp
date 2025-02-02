dd261e2a78b309fd69555e870063d80f1e2a5053
seongwonjung <122601269+seongwonjung@users.noreply.github.com>
2025-02-03 01:24:50 +0900

    Update 24479_알고리즘_수업-깊이_우선_탐색_1.cpp

--- SeongWon/Baekjoon/24479_알고리즘_수업-깊이_우선_탐색_1.cpp
+++ SeongWon/Baekjoon/24479_알고리즘_수업-깊이_우선_탐색_1.cpp
@@ -4,14 +4,16 @@
 using namespace std;
 vector<vector<int>> Graph;
 vector<bool> visited;
-void DFS(int r){
+vector<int> orders;
+void DFS(int r, int &order){
     visited[r] = true;  //방문 처리
-    cout << r << '\n';
+    orders[r] = order;
+    order++;
     int x;
-    for(int i = 0; i < Graph[r].size(); i++){
+    for(size_t i = 0; i < Graph[r].size(); i++){
         x = Graph[r][i];
         if(!visited[x]){    //방문하지 않은 노드 DFS재귀 호출
-            DFS(x);
+            DFS(x, order);
         }
     }
 }
@@ -22,23 +24,21 @@ int main() {
     cin >> n >> m >> r;
     Graph.assign(n+1, vector<int> (0,0));
     visited.assign(n+1, false);
+    orders.assign(n+1, false);
     for(int i = 0; i < m; i++) {
         int a, b;
         cin >> a >> b;
         Graph[a].emplace_back(b);
-        Graph[b].push_back(a);
+        Graph[b].emplace_back(a);
     }
     for(auto &arr : Graph){
         sort(arr.begin(), arr.end());
     }
-    DFS(r);
-    if(find(visited.begin(), visited.end(), false) != visited.end()){
-        cout << "0";
+    int order = 1;
+    DFS(r, order);
+    for(size_t i = 1; i <= n; i++){
+        cout << orders[i] << "\n";
     }
-    // for(auto a : Graph){
-    //     for(auto b : a){
-    //         cout << "b" << " ";
-    //     }
-    // }
+    
     return 0;
 }
