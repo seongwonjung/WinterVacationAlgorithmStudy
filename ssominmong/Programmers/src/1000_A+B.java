0b228f1d194200be13299dc3477acf8a7892465d
mincheol. bae <kimmincheol99@gmail.com>
2025-03-19 20:01:12 +0900

    Update 1000_A+B.java

--- ssominmong/Programmers/src/1000_A+B.java
+++ ssominmong/Programmers/src/1000_A+B.java
@@ -1,7 +1,7 @@
 //TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
 // 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
 import java.util.*;
-public class Main {
+ class g {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt(), b = sc.nextInt();
