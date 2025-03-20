1d7f1ebeaef1b03450d942ed743b59f83f2f7235
mincheol. bae <kimmincheol99@gmail.com>
2025-03-20 22:10:41 +0900

    .

--- ssominmong/Programmers/src/10699_오늘 날짜.java
+++ ssominmong/Programmers/src/10699_오늘 날짜.java
@@ -1,6 +1,6 @@
 import java.time.LocalDate;
 
-public class Today {
+class Today {
     public static void main(String[] args) {
         LocalDate today = LocalDate.now();
         System.out.println(today);
--- ssominmong/Programmers/src/10998_AXB.java
+++ ssominmong/Programmers/src/10998_AXB.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Multiply {
+class Multiply {
     public static void main(String[]args) {
         Scanner sc = new Scanner(System.in);
         int A = sc.nextInt();
