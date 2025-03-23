3154c479d71e7bd982da4fbe4207b9063519f875
mincheol. bae <kimmincheol99@gmail.com>
2025-03-23 19:50:41 +0900

    Add Bronze5-숫자 합

---
+++ ssominmong/Programmers/src/SumOfDigit.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+public class SumOfDigit {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bf.readLine());
+        String numbers = bf.readLine();
+
+        int sum = 0;
+        for(int i = 0; i < N; i++) {
+            sum += numbers.charAt(i)-'0';
+        }
+
+        System.out.println(sum);
+        bf.close();
+    }
+}
