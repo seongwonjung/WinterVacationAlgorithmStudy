865eb1f10c85e6a913d3f957e60299ab0ec69cc2
mincheol. bae <kimmincheol99@gmail.com>
2025-05-10 20:07:53 +0900

    .

--- ssominmong/Baekjoon/src/test.java
+++ ssominmong/Baekjoon/src/test.java
@@ -1,6 +1,24 @@
-import java.util.Arrays;
-import java.util.Collection;
+import java.util.*;
+import java.util.stream.*;
 
 public class test {
 
+    public static int[] solution(int[] numbers) {
+        Set<Integer> set = new HashSet<>();
+
+        for (int i = 0; i < numbers.length - 1; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                set.add(numbers[i] + numbers[j]);
+            }
+        }
+
+        return set.stream()
+                .sorted()
+                .mapToInt(Integer::intValue)
+                .toArray();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(solution(new int[]{2, 1, 3, 4, 1})));
+    }
 }
