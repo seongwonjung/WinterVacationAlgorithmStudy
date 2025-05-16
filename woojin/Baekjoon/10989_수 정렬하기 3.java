c182113d77ffe7b9d072c05437f94ae596f711b0
w00jinLee <leewoojin001121@gmail.com>
2025-05-16 19:49:59 +0900

    Bronze1-10989(수 정렬하기 3)

---
+++ woojin/Baekjoon/10989_수 정렬하기 3.java
@@ -0,0 +1,29 @@
+package woojin.Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class bj_10989 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[10001];
+
+        for(int i=0; i<N; i++){
+            int num=Integer.parseInt(br.readLine());
+            arr[num]++;
+        }
+
+        for(int i=0; i<arr.length; i++){
+            if(arr[i]>0){
+                for(int j=0; j<arr[i]; j++){
+                    sb.append(i).append('\n');
+                }
+            }
+        }
+        System.out.println(sb);
+    }
+}
