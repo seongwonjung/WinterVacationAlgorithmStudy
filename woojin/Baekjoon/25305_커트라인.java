6cf3269ae923613779271c037e4a2301aea3c3fa
w00jinLee <leewoojin001121@gmail.com>
2025-05-17 18:35:48 +0900

    Bronze2-25305(커트라인)

---
+++ woojin/Baekjoon/25305_커트라인.java
@@ -0,0 +1,23 @@
+package woojin.Baekjoon;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+class bj_25305 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int k = sc.nextInt();
+        int arr[] = new int[N];
+
+        for(int i=0; i<N; i++){
+            arr[i] = sc.nextInt();
+        }
+        
+        Arrays.sort(arr);
+
+        System.out.println(arr[N-k]);
+        sc.close();
+    }
+}
